b423e4a706538fd5d0cc20936cb88825800cc756
johnnieskywalker <lwolski0@gmail.com>
2024-02-03 18:28:26 +0100

    unified imports

--- test/AiFaces.t.sol
+++ test/AiFaces.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "forge-std/Test.sol";
+import {Test} from "../lib/forge-std/src/Test.sol";
 import "../src/AiFaces.sol";
 
 contract AiFacesTest is Test {
