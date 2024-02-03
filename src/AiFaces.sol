a56d6c257f6ba54f452c8fb38c73ab70ec5d6bfc
johnnieskywalker <lwolski0@gmail.com>
2024-02-03 17:56:40 +0100

    fixed import vs code issue

--- src/AiFaces.sol
+++ src/AiFaces.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.8.19;
 
-import "ERC721A/ERC721A.sol";
+import {ERC721A} from "../lib/ERC721A/contracts/ERC721A.sol";
 
 contract AiFaces is ERC721A {
-    constructor() ERC721A("MyNFTCollection", "MNFT") {}
+    constructor() ERC721A("AiFaces", "AIF") {}
 
     function mint(uint256 quantity) public {
         _mint(msg.sender, quantity);
