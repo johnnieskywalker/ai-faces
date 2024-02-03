6a48a76d86f0890ebe43a404221bb5f3b4e7ba86
johnnieskywalker <lwolski0@gmail.com>
2024-02-03 18:44:01 +0100

    added test for token URI

--- src/AiFaces.sol
+++ src/AiFaces.sol
@@ -4,13 +4,15 @@ pragma solidity 0.8.19;
 import {ERC721A} from "../lib/ERC721A/contracts/ERC721A.sol";
 
 contract AiFaces is ERC721A {
-    constructor() ERC721A("AiFaces", "MNFT") {}
+    string public constant BASE_URI = "https://github.com/johnnieskywalker/ai-faces/tree/main/pictures/";
+
+    constructor() ERC721A("AiFaces", "AIFA") {}
 
     function mint(uint256 quantity) public {
         _mint(msg.sender, quantity);
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
-        return "ipfs://YourBaseURIHere/";
+        return BASE_URI;
     }
 }
--- test/AiFaces.t.sol
+++ test/AiFaces.t.sol
@@ -15,4 +15,11 @@ contract AiFacesTest is Test {
         myNFT.mint(1);
         assertEq(myNFT.balanceOf(address(this)), 1);
     }
+
+    function testTokenURI() public {
+        myNFT.mint(1);
+        string memory expectedTokenURI = "https://github.com/johnnieskywalker/ai-faces/tree/main/pictures/0";
+        string memory actualTokenURI = myNFT.tokenURI(0);
+        assertEq(actualTokenURI, expectedTokenURI, "The tokenURI does not match the expected value.");
+    }
 }
